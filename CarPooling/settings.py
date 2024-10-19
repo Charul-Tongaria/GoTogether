25330084c24943e5cc4fc891d0c8590d58d65800
Charul Tongaria <charultongaria.1703@gmail.com>
2024-10-20 00:53:08 +0530

    Update settings.py

--- CarPooling/settings.py
+++ CarPooling/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-62u!tjn_4-u^v5(o=6hipyx#7yq&&(hwcpn!mqi27h4tdkr_*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
